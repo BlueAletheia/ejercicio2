ca74adfc2adcc71e3fd8781fe0398bd38ddc379d
Alicia Fernandez <alicia.fernandez@bosonit.com>
2022-07-08 13:53:57 +0200

    resta

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function suma (a ,b){
+    return a+b;
+}
+function resta (a ,b){
+    return a-b;
+}
+const val1 = prompt("Valor 1: ", 0);
+const val2 = prompt("Valor 2: ", 0);
+console.log(suma(val1, val2));
+console.log(resta(val1, val2));
